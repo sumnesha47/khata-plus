561ffc8b430e640374ad9bd448d52fad3e9d5fed
Sumnesh Agrawal <sumnesha47@gmail.com>
2026-05-28 11:14:44 +0530

    bump up version to 0.0.5

--- electrobun.config.ts
+++ electrobun.config.ts
@@ -4,7 +4,7 @@ export default {
 	app: {
 		name: "khata-plus",
 		identifier: "khataplus.electrobun.dev",
-		version: "0.0.4",
+		version: "0.0.5",
 	},
 	build: {
 		// Vite builds to dist/, we copy from there
